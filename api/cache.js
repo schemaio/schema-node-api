b792b2a4e3631b9d8624b477fc7f9c8786f8257d
Eric Ingram <eric@schema.io>
2016-10-24 07:44:04 -0700

    Make cache optional

--- api/cache.js
+++ api/cache.js
@@ -2,7 +2,7 @@ const apicache = require('apicache');
 
 module.exports = (env) => {
   if (!env.CACHE_SECONDS) {
-    return;
+    return (req, res, next) => next();
   }
   return apicache.options({
     debug: env.NODE_ENV === 'development',
